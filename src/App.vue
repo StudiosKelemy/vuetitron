55f18a7e4c32235c8a17a412a2df0278af4491b0
CodeKelemy <studios.kelemy@gmail.com>
2020-08-27 13:49:02 +0000

    vuetitron configuration

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
         <v-app-bar app color="primary" dark>
-            Lorems ipsum dolor sit amet consectetur adipisicing elit. Recusandae rem ipsa nemo impedit atque animi, laborum architecto eum, consequuntur, facilis sint vel beatae est error sapiente quibusdam unde minus iste.
+            Lorems ipsums dolor sit amet consectetur adipisicing elit. Recusandae rem ipsa nemo impedit atque animi, laborum architecto eum, consequuntur, facilis sint vel beatae est error sapiente quibusdam unde minus iste.
         </v-app-bar>
 
         <v-main>
